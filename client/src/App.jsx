37cd01582f1b156ef0723e3745321484bf820505
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-04-03 23:01:34 +0000

    setting chadcn and tailwind for frontend preparation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,11 @@
-export default function App() {
-  return <div>App</div>;
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
+
+export default App;
