8a25b7f76b805af60ab9510f76c203432d59d585
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-04-05 18:37:18 +0000

    Finish the static collection route

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -9,8 +9,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
 
 export default function Collection() {
+  const [sorting, setSorting] = useState("relevant");
+
   return (
     <div className="flex flex-col md:flex-row w-full py-5">
       <div className="w-full md:w-1/4 pr-5">
@@ -81,7 +84,7 @@ export default function Collection() {
           <h1 className="font-bold text-xl relative after:content-[''] after:absolute after:-right-11 after:top-1/2 after:w-10 after:h-[2px] after:bg-black">
             <span className="text-slate-500">ALL</span> COLLECTIONS
           </h1>
-          <Select>
+          <Select onValueChange={(e) => setSorting(e)}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Sort By: Relevant" />
             </SelectTrigger>
