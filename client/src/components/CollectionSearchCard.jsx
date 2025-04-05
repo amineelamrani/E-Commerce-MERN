3b4a92ed72014710d91b601c199229e223d0af5a
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-04-05 09:20:37 +0000

    Collection page

--- client/src/components/CollectionSearchCard.jsx
+++ client/src/components/CollectionSearchCard.jsx
@@ -1,8 +1,8 @@
 import testImage from "../assets/p_img47.png";
 
-export default function CollectionItemCard({ title, price }) {
+export default function CollectionSearchCard({ title, price }) {
   return (
-    <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-2">
+    <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2">
       <div className="flex flex-col">
         <img src={testImage} alt="" className="w-full" />
         <h1 className="py-2">{title}</h1>
--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,5 +1,107 @@
-import React from "react";
+import CollectionSearchCard from "@/components/CollectionSearchCard";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Collection() {
-  return <div>Collection</div>;
+  return (
+    <div className="flex flex-col md:flex-row w-full py-5">
+      <div className="w-full md:w-1/4 pr-5">
+        <h1 className="py-5 font-bold text-xl">FILTERS</h1>
+        <div className="w-full border rounded-md p-3 flex flex-col mb-5">
+          <h2 className="pb-2 font-bold">CATEGORIES</h2>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              MEN
+            </label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              WOMEN
+            </label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              KIDS
+            </label>
+          </div>
+        </div>
+        <div className="w-full border rounded-md p-3 flex flex-col">
+          <h2 className="pb-2 font-bold">TYPE</h2>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              TopWear
+            </label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              BottomWear
+            </label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              WinterWear
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:3/4 pl-5">
+        <div className="py-5 flex justify-between items-center px-3">
+          <h1 className="font-bold text-xl relative after:content-[''] after:absolute after:-right-11 after:top-1/2 after:w-10 after:h-[2px] after:bg-black">
+            <span className="text-slate-500">ALL</span> COLLECTIONS
+          </h1>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Sort By: Relevant" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Sort By :</SelectLabel>
+                <SelectItem value="relevant">Sort By: Relevant</SelectItem>
+                <SelectItem value="lowest">Sort By: Low to High</SelectItem>
+                <SelectItem value="highest">Sort By: High to Low</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="w-full flex flex-wrap">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <CollectionSearchCard title={"HEllo"} price={45} key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
