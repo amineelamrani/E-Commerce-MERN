1be24909b92f08682c8aa702edbf4d494782e636
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-04-08 23:18:05 +0100

    Collection page done

--- client/src/components/CollectionSearchCard.jsx
+++ client/src/components/CollectionSearchCard.jsx
@@ -1,10 +1,14 @@
-import testImage from "../assets/p_img47.png";
-
-export default function CollectionSearchCard({ title, price }) {
+export default function CollectionSearchCard({ title, price, image }) {
   return (
     <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2">
       <div className="flex flex-col">
-        <img src={testImage} alt="" className="w-full" />
+        <div className="overflow-hidden">
+          <img
+            src={image}
+            alt=""
+            className="w-full hover:cursor-pointer hover:scale-110 transition-transform duration-200"
+          />
+        </div>
         <h1 className="py-2">{title}</h1>
         <h2>${price}</h2>
       </div>
--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import CollectionSearchCard from "@/components/CollectionSearchCard";
 import { Checkbox } from "@/components/ui/checkbox";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import {
   Select,
   SelectContent,
@@ -9,10 +10,110 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Collection() {
   const [sorting, setSorting] = useState("relevant");
+  const [fetchedProducts, setFetchedProducts] = useState(null);
+  const [categories, setCategories] = useState({
+    Men: true,
+    Women: true,
+    Kids: true,
+  });
+  const [types, setTypes] = useState({
+    Topwear: true,
+    Bottomwear: true,
+    Winterwear: true,
+  });
+  let sortedProducts = fetchedProducts !== null ? [...fetchedProducts] : null;
+  let searchCardsContent = (
+    <LoadingSpinner className="my-5 h-14 w-14 mx-auto" />
+  );
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("/api/v1/products/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (data !== null && data.status === "success") {
+        setFetchedProducts(data.result);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  function sortByPriceLowToHigh(items) {
+    items.sort((a, b) => a.price - b.price);
+  }
+  function sortByPriceHighToLow(items) {
+    items.sort((a, b) => b.price - a.price);
+  }
+
+  if (fetchedProducts !== null) {
+    if (sorting === "relevant") {
+      sortedProducts = [...fetchedProducts];
+    } else if (sorting === "lowest") {
+      sortedProducts = [...fetchedProducts];
+      sortByPriceLowToHigh(sortedProducts);
+    } else if (sorting === "highest") {
+      sortedProducts = [...fetchedProducts];
+      sortByPriceHighToLow(sortedProducts);
+    }
+  }
+
+  const handleCheckboxCatChange = (checked, id) => {
+    setCategories({ ...categories, [id]: checked });
+  };
+  const handleCheckboxTypeChange = (checked, id) => {
+    setTypes({ ...types, [id]: checked });
+  };
+
+  if (fetchedProducts !== null) {
+    const { Topwear, Bottomwear, Winterwear } = types;
+    searchCardsContent = sortedProducts.map((product, index) => {
+      for (const [key, value] of Object.entries(categories)) {
+        if (value && product.category.includes(key)) {
+          // check the types now
+          if (Topwear && Bottomwear && Winterwear) {
+            return (
+              <CollectionSearchCard
+                title={product.title}
+                price={product.price}
+                image={product.images[0]}
+                key={index}
+              />
+            );
+          } else if (!Topwear && !Bottomwear && !Winterwear) {
+            return;
+          }
+
+          for (const [keyType, valueType] of Object.entries(types)) {
+            if (valueType && product.subCategory.includes(keyType)) {
+              return (
+                <CollectionSearchCard
+                  title={product.title}
+                  price={product.price}
+                  image={product.images[0]}
+                  key={index}
+                />
+              );
+            }
+          }
+        }
+      }
+    });
+    if (searchCardsContent.filter((item) => item !== undefined).length === 0) {
+      searchCardsContent = (
+        <h1 className="text-xl text-red-500 italic my-1 mx-auto">
+          No Item Found!
+        </h1>
+      );
+    }
+  }
 
   return (
     <div className="flex flex-col md:flex-row w-full py-5">
@@ -21,27 +122,45 @@ export default function Collection() {
         <div className="w-full border rounded-md p-3 flex flex-col mb-5">
           <h2 className="pb-2 font-bold">CATEGORIES</h2>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Men"
+              checked={categories.Men}
+              onCheckedChange={(checked) =>
+                handleCheckboxCatChange(checked, "Men")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Men"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               MEN
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Women"
+              checked={categories.Women}
+              onCheckedChange={(checked) =>
+                handleCheckboxCatChange(checked, "Women")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Women"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               WOMEN
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Kids"
+              checked={categories.Kids}
+              onCheckedChange={(checked) =>
+                handleCheckboxCatChange(checked, "Kids")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Kids"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               KIDS
@@ -51,27 +170,45 @@ export default function Collection() {
         <div className="w-full border rounded-md p-3 flex flex-col">
           <h2 className="pb-2 font-bold">TYPE</h2>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Topwear"
+              checked={types.Topwear}
+              onCheckedChange={(checked) =>
+                handleCheckboxTypeChange(checked, "Topwear")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Topwear"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               TopWear
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Bottomwear"
+              checked={types.Bottomwear}
+              onCheckedChange={(checked) =>
+                handleCheckboxTypeChange(checked, "Bottomwear")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Bottomwear"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               BottomWear
             </label>
           </div>
           <div className="flex items-center space-x-2">
-            <Checkbox id="terms" />
+            <Checkbox
+              id="Winterwear"
+              checked={types.Winterwear}
+              onCheckedChange={(checked) =>
+                handleCheckboxTypeChange(checked, "Winterwear")
+              }
+            />
             <label
-              htmlFor="terms"
+              htmlFor="Winterwear"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               WinterWear
@@ -80,8 +217,8 @@ export default function Collection() {
         </div>
       </div>
       <div className="w-full md:3/4 pl-5">
-        <div className="py-5 flex justify-between items-center px-3">
-          <h1 className="font-bold text-xl relative after:content-[''] after:absolute after:-right-11 after:top-1/2 after:w-10 after:h-[2px] after:bg-black">
+        <div className="py-5 flex flex-col sm:flex-row justify-between items-end sm:items-center px-3">
+          <h1 className="font-bold text-xl relative self-start after:content-[''] after:absolute after:-right-11 after:top-1/2 after:w-10 after:h-[2px] after:bg-black">
             <span className="text-slate-500">ALL</span> COLLECTIONS
           </h1>
           <Select onValueChange={(e) => setSorting(e)}>
@@ -99,10 +236,8 @@ export default function Collection() {
           </Select>
         </div>
 
-        <div className="w-full flex flex-wrap">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <CollectionSearchCard title={"HEllo"} price={45} key={index} />
-          ))}
+        <div className="w-full flex flex-wrap items-start">
+          {searchCardsContent}
         </div>
       </div>
     </div>
