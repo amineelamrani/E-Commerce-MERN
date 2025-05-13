36062678c7c3c121329cdc8c9f49fe970654b257
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-05-13 23:21:55 +0100

    fix the issue of synchronisation of reviews number when scrolling from a product to another product

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -100,7 +100,7 @@ export default function Product() {
     };
 
     fetchReviewsData();
-  }, [isProductReviewed]);
+  }, [isProductReviewed, productID]);
 
   const handleImageClick = (e) => {
     setHighlightedImage(e.target.currentSrc);
